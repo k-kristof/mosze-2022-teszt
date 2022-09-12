5b3bb92e8379609f1da06633e3ea53beb41db3ee
Kristóf Káldy <kaldykristof@gmail.com>
2022-09-12 19:06:56 +0200

    add comments

--- main.cpp
+++ main.cpp
@@ -4,17 +4,23 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
+    // Hibás változó név
     int *b = new int[NELEMENTS];
+    // Hibás cout szintaxis
     std::cout << '1-100 ertekek duplazasa'
+    // Befejezetlen for ciklus
     for (int i = 0;)
     {
         b[i] = i * 2;
     }
+    // Befejezetlen for ciklus
     for (int i = 0; i; i++)
     {
+        // Hibás cout szintaxis
         std::cout << "Ertek:"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
+    // Inicializálatlan változó
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
